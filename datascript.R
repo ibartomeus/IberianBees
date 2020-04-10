77a21b23642e4e1e7527c9c756f8c696a1f5aaeb
ibartomeus <nach@home>
2020-04-11 00:40:34 +0200

    Update notes

--- datascript.R
+++ datascript.R
@@ -1,1 +1,16 @@
-print("Have this script run whatever data cleaning you do")
+#Update metadata
+#At least summary of:
+#Name	Contact	Number of records as main dataholder	Number of records as co-dataholder
+#Maybe also: 
+
+#things to do:
+
+#flowers species with Genus_spcies -> change in bulk.
+#newdat$Reference..doi. several doi's listed "," and "and" separated. Fix later? What to do with dois?
+#remove all NA rows
+#remove duplicates?
+#create column of trusted / untrusted.
+#summaries -> how to separate authors?
+
+
+  
---
+++ rawdata/preprocessing.R
@@ -0,0 +1,18 @@
+#This file is internal and converts xls to csv's to make the repo lightweight.
+library(openxlsx)
+library(filesstrings)
+
+#xls files should be properly names, and with the data in the first sheet.
+# Create a vector of Excel files to read
+files.to.read = list.files(path = "rawdata/xls_to_add", pattern="xlsx")
+
+# Read each file and write it to csv
+lapply(files.to.read, function(f) {
+  df = read.xlsx(xlsxFile = paste("rawdata/xlss/",f, sep = ""), sheet=1)
+  write.csv(df, paste("rawdata/", gsub("xlsx", "csv", f), sep = ""), row.names=FALSE)
+})
+
+#move xls's
+file.move(files = paste("rawdata/xls_to_add/", files.to.read, sep = ""), 
+          destinations = "rawdata/xls_added")
+
