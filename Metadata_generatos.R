efc3b27f005858f014cc7138bef2d2e6daa624d6
ibartomeus <nach@home>
2021-08-30 13:26:42 +0200

    Create EML

--- Metadata_generatos.R
+++ Metadata_generatos.R
@@ -1,4 +1,46 @@
-#Test EML
+# Metadata is created using dataspice ----
+
+#RUN JUST ONCE! As involves manual data entry via shiny apps.
+#install.packages("dataspice")
+library(dataspice)
+create_spice() #creates metadata directory
+# Then fill in template CSV files, more on this below
+files <- "~/Documents/R/IberianBees/data/data_clean.csv"
+attributes_path <- file.path("data", "metadata", "attributes.csv")
+files %>%
+  purrr::map(~ prep_attributes(.x, attributes_path),
+             attributes_path = attributes_path
+  )
+edit_attributes()
+edit_access()
+edit_creators()
+edit_biblio()
+
+write_spice() #creates Json Original name is dataspice.json. Manually renamed to IBD.json
+
+build_site() # Optional
+
+#create EML (and validate it)
+IBD_json <-  "~/Documents/R/IberianBees/data/metadata/IBD.json"
+eml_doc <- spice_to_eml(IBD_json)
+library(EML)
+eml_validate(eml_doc)
+eml_doc$packageId <- uuid::UUIDgenerate()
+eml_doc$system <- "uuid"
+eml_validate(eml_doc) #need to fix those...
+
+#write EML
+write_eml(eml_doc, "data/metadata/IBD.xml")
+
+
+
+
+
+
+
+
+
+# I also exmplred EML package... but thi sis not used-----
 
 #install.packages("EML")
 library(EML)
@@ -69,5 +111,7 @@ my_eml <- eml$eml(
 
 
 eml_validate(IBD_eml)
-write_eml(IBD_eml, "IBD.xml")
+#write_eml(IBD_eml, "IBD.xml")
+
+
 
