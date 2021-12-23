373dc773b20b49c4fdd1d1f4e3e737b1a88cb4da
Jose Barragán Sánchez-Lanuza <lanuza@Joses-MacBook-Pro.local>
2021-12-23 17:00:37 +0100

    Small edit on path

--- rawdata/Rename_csvs.R
+++ rawdata/Rename_csvs.R
@@ -8,7 +8,7 @@
 #are more authors involved
 
 #Read all files
-file_names_old <- list.files("~/R_projects/IberianBees/rawdata/csvs")              # Get current file names
+file_names_old <- list.files("./rawdata/csvs")              # Get current file names
 #write.csv(file_names_old, "Rename_csv.csv") #Save file names to add
 #names manually, this needs a proper search in the dataset and check
 #who is the main contributor of the dataset, that is, 
@@ -17,11 +17,11 @@ file_names_old <- list.files("~/R_projects/IberianBees/rawdata/csvs")           
 
 #Once this previous task is done and the new names are added manually
 #we proceed to read the data and rename after
-file_names_new <- read.csv("~/R_projects/IberianBees/rawdata/Rename_csvs.csv")
+file_names_new <- read.csv("./rawdata/Rename_csvs.csv")
 file_names_new <- file_names_new$New_Name #Overwrite with list of new names
   
 #Set path name and rename
-my_path <- "~/R_projects/IberianBees/rawdata/csvs/"
+my_path <- "./rawdata/csvs/"
 # Rename files  
 file.rename(paste0(my_path, file_names_old),      
             paste0(my_path, file_names_new))            
