7b58b85db19606b1db1d5e00b6922cfd622ac42c
Jose Barragán Sánchez-Lanuza <lanuza@Joses-MacBook-Pro.local>
2021-12-26 19:11:39 +0100

    19_Kuhlmann_etal ready

--- rawdata/Add_data.R
+++ rawdata/Add_data.R
@@ -778,14 +778,13 @@ newdat$Collector <- gsub("H.Gaspar", "H. Gaspar", newdat$Collector)
 newdat$Collector <- gsub("H. Gaspar & P.Ferreira", 
 "H. Gaspar, P.Ferreira", newdat$Collector)
 
-
 write.table(x = newdat, file = "data/data.csv", 
             quote = TRUE, sep = ",", col.names = FALSE,
             row.names = FALSE, append = TRUE)
 size <- size + nrow(newdat)
 
 #####################################################################################
-# 19_Castro_etal
+# 19_Kuhlmann_etal
 
 #help_structure()
 newdat <- read.csv(file = 'rawdata/csvs/19_Kuhlmann_etal.csv', sep = ";")
@@ -801,6 +800,53 @@ newdat$Longitude <- parzer::parse_lon(as.character(newdat$Longitude))
 newdat <- drop_variables(check, newdat) #reorder and drop variables
 summary(newdat)
 newdat <- add_uid(newdat = newdat, '19_Kuhlmann_etal_')
+
+#Rename Country
+newdat$Country <- gsub("SPAIN", "Spain", newdat$Country)
+
+#Extract year and month and fill
+#Fix now dates, some years missing that can be filled from start and end date
+#Extract year from strat.date column, store it another dataframe
+year_d <- as.data.frame(format(as.Date(newdat$Start.date, format="%d-%m-%Y"),"%Y"))
+colnames(year_d) <- "y" #New colname for simplicity
+year_d$Year <- newdat$Year #Add new column (the year one from newdat)
+#Workaround to fill missing years (needs Tydiverse)
+year_d_1 <- data.frame(t(year_d)) %>% 
+  fill(., names(.)) %>%
+  t() %>% as.data.frame()
+#Works well, add now the column back to the dataframe
+newdat$Year <- year_d_1$Year
+#Check levels, fix "   6", its year 2008
+levels(factor(newdat$Year))
+
+#Now this process can be repeated by month
+#Extract month from start.date column, store it another dataframe
+month_d <- as.data.frame(format(as.Date(newdat$Start.date, format="%d-%m-%Y"),"%m"))
+colnames(month_d) <- "m" #New colname for simplicity
+#Add leading 0 to month column before merging
+newdat$Month <- ifelse(newdat$Month < 10, paste0("0", newdat$Month), newdat$Month)
+month_d$Month <- newdat$Month #Add new column (the month one from newdat)
+#Workaround to fill missing years (needs Tydiverse)
+month_d_1 <- data.frame(t(month_d)) %>% 
+  fill(., names(.)) %>%
+  t() %>% as.data.frame()
+#Works well, add now the column back to the dataframe
+newdat$Month <- month_d_1$Month
+
+#The number of levels here is a bit crazy
+#and maybe a bit repetitive
+levels(factor(newdat$Authors.to.give.credit))
+#just convert to lower case these ones
+newdat$Authors.to.give.credit <- gsub("(NOSKIEWICZ 1936)", 
+"Noskiewicz 1936", newdat$Authors.to.give.credit, fixed=T)
+newdat$Authors.to.give.credit <- gsub("(RATHJEN 1998)",
+"Rathjen 1998", newdat$Authors.to.give.credit, fixed=T)
+newdat$Authors.to.give.credit <- gsub("(WARNCKE 1978)",
+"Warncke 1978", newdat$Authors.to.give.credit, fixed=T)
+newdat$Authors.to.give.credit <- gsub("(WESTRICH 1997)",
+"Westrich 1997", newdat$Authors.to.give.credit, fixed=T)
+
+
 write.table(x = newdat, file = 'data/data.csv', 
     quote = TRUE, sep = ',', col.names = FALSE, 
     row.names = FALSE, append = TRUE)
