d6b0a56f77801d757c4cd4f523ed87232bfd1f5b
Jose Barragán Sánchez-Lanuza <barragansljose@gmail.com>
2021-12-27 13:56:53 +0100

    25_Trillo ready

--- rawdata/Add_data.R
+++ rawdata/Add_data.R
@@ -1024,7 +1024,20 @@ compare_variables(check, newdat)
 newdat <- add_missing_variables(check, newdat)
 newdat <- drop_variables(check, newdat) #reorder and drop variables
 summary(newdat)
+
+#Add leading 0 to month
+newdat$Month <- ifelse(newdat$Month < 10, paste0("0", newdat$Month), newdat$Month)
+newdat$Day <- ifelse(newdat$Day < 10, paste0("0", newdat$Day), newdat$Day)
+#Standardize separator
+levels(factor(newdat$Determined.by))
+newdat$Determined.by <- gsub("FJ Ortiz-Sánchez", 
+"F.J. Ortiz-Sánchez",newdat$Determined.by)
+newdat$Authors.to.give.credit <- gsub("A. Trillo, FJ Ortiz-Sánchez", 
+"A. Trillo, F.J. Ortiz-Sánchez",newdat$Authors.to.give.credit)
+
+#Add unique identifier
 newdat <- add_uid(newdat = newdat, '25_Trillo_')
+
 write.table(x = newdat, file = 'data/data.csv', 
     quote = TRUE, sep = ',', col.names = FALSE, 
     row.names = FALSE, append = TRUE)
