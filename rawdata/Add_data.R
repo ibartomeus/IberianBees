fa0eea81afa8f39cc5a1d521c0887cb82eaa9f05
Jose Barragán Sánchez-Lanuza <barragansljose@gmail.com>
2021-12-27 17:11:21 +0100

    31_Diaz-Calafat ready

--- rawdata/Add_data.R
+++ rawdata/Add_data.R
@@ -1244,7 +1244,14 @@ compare_variables(check, newdat)
 newdat <- add_missing_variables(check, newdat)
 newdat <- drop_variables(check, newdat) #reorder and drop variables
 summary(newdat)
+
+#Add leading 0 to month
+newdat$Month <- ifelse(newdat$Month < 10, paste0("0", newdat$Month), newdat$Month)
+newdat$Day <- ifelse(newdat$Day < 10, paste0("0", newdat$Day), newdat$Day)
+
+#Add unique identifier
 newdat <- add_uid(newdat = newdat, '31_Diaz-Calafat_')
+
 write.table(x = newdat, file = 'data/data.csv', 
     quote = TRUE, sep = ',', col.names = FALSE, 
     row.names = FALSE, append = TRUE)
