223e50809547475e2e89f687d1e74b4ff3c26116
Jose Barragán Sánchez-Lanuza <barragansljose@gmail.com>
2021-12-27 15:14:32 +0100

    29_Hormaza_etal ready

--- rawdata/Add_data.R
+++ rawdata/Add_data.R
@@ -1161,7 +1161,25 @@ compare_variables(check, newdat)
 newdat <- add_missing_variables(check, newdat)
 newdat <- drop_variables(check, newdat) #reorder and drop variables
 summary(newdat)
+
+#Few values are missing and seem the same of the rest of the columns
+newdat$Country <- "Spain"
+newdat$Province <- "Malaga"
+newdat$Locality <- "Algarrobo"
+newdat$Latitude <- 36.759
+newdat$Longitude <- -4.04
+newdat$Year <- 2018
+newdat$Month <- 04
+newdat$Collector <- "O. Aguado"
+newdat$Determined.by <- "O. Aguado"
+newdat$Authors.to.give.credit <- "O. Aguado, J.I. Hormaza, M.L. Alcaraz, V. Ferrero"
+
+#Delete row with NA in genus
+newdat <- newdat[!is.na(newdat$Genus),]
+
+#Add unique identifier
 newdat <- add_uid(newdat = newdat, '29_Hormaza_etal_')
+
 write.table(x = newdat, file = 'data/data.csv', 
     quote = TRUE, sep = ',', col.names = FALSE, 
     row.names = FALSE, append = TRUE)
