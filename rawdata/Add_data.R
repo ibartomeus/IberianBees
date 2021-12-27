08c7926ed53f2cd58cea4582407d5918e606e8ee
Jose Barragán Sánchez-Lanuza <lanuza@Joses-MacBook-Pro.local>
2021-12-27 22:12:29 +0100

    38_Ortiz_etal ready

--- rawdata/Add_data.R
+++ rawdata/Add_data.R
@@ -1588,8 +1588,43 @@ summary(newdat)
 newdat$Female <- as.character(newdat$Female)
 newdat$Worker[which(newdat$Female == " obrera")] <- 1
 newdat$Female[which(newdat$Female == " obrera")] <- 0
-newdat$Authors.to.give.credit <- "Ortiz, Torres, Ornosa"
+newdat$Authors.to.give.credit <- "J. Ortiz, F. Torres, C. Ornosa"
 newdat <- add_uid(newdat = newdat, '38_Ortiz_etal_')
+
+#Rename country
+newdat$Country <- gsub("España", "Spain", newdat$Country)
+#Fix province
+newdat$Province <- gsub("Á06la", "Ávila", newdat$Province)
+newdat$Province <- gsub("Sego06a", "Segovia", newdat$Province)
+newdat$Province <- gsub("Albacetel", "Albacete", newdat$Province)
+newdat$Province <- gsub("Lérida", "Lleida", newdat$Province)
+newdat$Province <- gsub("Sierra Gádor", "Almería", newdat$Province)
+#Fix localities
+levels(factor(newdat$Locality))
+newdat$Locality <- gsub("Cortijo de la Cruz. Dalías", 
+"Sierra Gádor. Cortijo de la Cruz. Dalías", newdat$Locality)
+newdat$Locality <- gsub("\\006", 
+"vi", newdat$Locality)
+
+#Add leading 0 to month
+newdat$Month <- ifelse(newdat$Month < 10, paste0("0", newdat$Month), newdat$Month)
+newdat$Day <- ifelse(newdat$Day < 10, paste0("0", newdat$Day), newdat$Day)
+
+#Standardize collectors
+newdat$Collector <- gsub("A  Vázquez", "A. Vázquez", newdat$Collector)
+newdat$Collector <- gsub("C. Ornosa/F Torres", "C. Ornosa, F. Torres", newdat$Collector  )
+newdat$Collector <- gsub("E  Manzano", "E. Manzano", newdat$Collector)
+newdat$Collector <- gsub("P. Vargas/JL Blanco", "P. Vargas, J.L. Blanco", newdat$Collector  )
+newdat$Collector <- gsub("Pablo Vargas", "P. Vargas", newdat$Collector  )
+newdat$Collector <- gsub("Jesús Gomez", "J. Gomez", newdat$Collector  )
+#Standardize determined.by
+newdat$Determined.by <- gsub("C Ornosa", "C. Ornosa", newdat$Determined.by)
+newdat$Determined.by <- gsub("F. Torres/ C. Ornosa", "F. Torres, C. Ornosa", newdat$Determined.by)
+newdat$Determined.by <- gsub("J.  Ortiz", "J. Ortiz", newdat$Determined.by)
+newdat$Determined.by <- gsub("J. Ortiz/C. Ornosa", "J. Ortiz, C. Ornosa", newdat$Determined.by)
+newdat$Determined.by <- gsub("Félix Torres", "F. Torres", newdat$Determined.by)
+levels(factor(newdat$Authors.to.give.credit))
+
 write.table(x = newdat, file = 'data/data.csv', 
     quote = TRUE, sep = ',', col.names = FALSE, 
     row.names = FALSE, append = TRUE)
