02099a3c78b3d1de6b82ab40c395330ecea057db
Jose Barragán Sánchez-Lanuza <barragansljose@gmail.com>
2021-12-28 14:00:42 +0100

    42_Ornosa_etal ready

--- rawdata/Add_data.R
+++ rawdata/Add_data.R
@@ -1752,6 +1752,30 @@ newdat <- newdat[,c(1,25,2:12,26,27,13:24,28)]#reorder
 #quick way to compare colnames
 cbind(colnames(newdat), colnames(data)) #can be merged
 summary(newdat)
+
+compare_variables(check, newdat)
+#Rename colnames
+colnames(newdat)[which(colnames(newdat)=="Coordinate.precision..e.g..GPS...10km.")] <- "Coordinate.precision"
+colnames(newdat)[which(colnames(newdat)=="month")] <- "Month"
+colnames(newdat)[which(colnames(newdat)=="day")] <- "Day"
+colnames(newdat)[which(colnames(newdat)=="Determiner")] <- "Determined.by"
+colnames(newdat)[which(colnames(newdat)=="Reference..doi.")] <- "Reference.doi"
+newdat <- drop_variables(check, newdat) #reorder and drop variables
+
+#Convert month to numeric
+newdat$Month <- match(newdat$Month, month.name)
+#Add leading 0 to month
+newdat$Month <- ifelse(newdat$Month < 10, paste0("0", newdat$Month), newdat$Month)
+newdat$Day <- ifelse(newdat$Day < 10, paste0("0", newdat$Day), newdat$Day)
+
+#Change separator in collector
+newdat$Collector <- gsub("\\ &", ",", newdat$Collector)
+
+#Fix DOI with extra dot
+newdat$Reference.doi <- gsub("https://doi.org/10/f3pm57.",
+     "https://doi.org/10/f3pm57", newdat$Reference.doi)
+#All DOI'S work fine now
+
 #write
 write.table(x = newdat, file = "data/data.csv", 
             quote = TRUE, sep = ",", col.names = FALSE,
