9e2f800deb27fadf4533882e508626c4bfd0d819
Jose Barragán Sánchez-Lanuza <barragansljose@gmail.com>
2021-12-29 18:06:59 +0100

    51_Minarro ready

--- rawdata/Add_data.R
+++ rawdata/Add_data.R
@@ -2455,6 +2455,29 @@ newdat <- newdat[,c(1,25,2:12,26,27,13:24,28)]
 cbind(colnames(newdat), colnames(data)) #can be merged
 summary(newdat)
 newdat$Authors.to.give.credit <- "M. Miñarro, A. Núñez"
+
+#Compare vars
+compare_variables(check, newdat)
+#Rename columns
+colnames(newdat)[which(colnames(newdat)=="Coordinate.precision..e.g..GPS...10km.")] <- "Coordinate.precision"
+colnames(newdat)[which(colnames(newdat)=="day")] <- "Day"
+colnames(newdat)[which(colnames(newdat)=="Reference..doi.")] <- "Reference.doi"
+colnames(newdat)[which(colnames(newdat)=="Collection.Location_ID")] <- "Local_ID"
+colnames(newdat)[which(colnames(newdat)=="End.Date")] <- "End.date"
+colnames(newdat)[which(colnames(newdat)=="month")] <- "Month"
+
+#Drop variables and reorder
+newdat <- drop_variables(check, newdat)
+
+#Add leading 0 to month
+newdat$Month <- ifelse(newdat$Month < 10, paste0("0", newdat$Month), newdat$Month)
+newdat$Day <- ifelse(newdat$Day < 10, paste0("0", newdat$Day), newdat$Day)
+
+#Add an extra space on collectors
+newdat$Collector <- gsub("\\.", ". ", newdat$Collector)
+newdat$Determined.by <- gsub("\\.", ". ", newdat$Determined.by)
+newdat$Determined.by <- gsub("Identified by A. Núñez, O.  Aguado and/or J.  Ortiz", "A. Núñez, O.  Aguado, J.  Ortiz", newdat$Determined.by)
+
 #write
 write.table(x = newdat, file = "data/data.csv", 
             quote = TRUE, sep = ",", col.names = FALSE,
