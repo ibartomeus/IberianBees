68ea9fc1564c87a3ea0530e8a066064cc125c750
Jose Barragán Sánchez-Lanuza <barragansljose@gmail.com>
2022-02-26 09:31:56 +0100

    File orgnization

--- Scripts/1_1_Fetch_data.R
+++ Scripts/1_1_Fetch_data.R
@@ -1,19 +1,14 @@
-#Fetch data from internet
 
 ########################################-
-# Download GBIF data ----
+# Download gbif data ----
 ########################################-
+
 #Load libraries
-library(rgbif)
-library(cleanR)
-library(tidyverse)
+library(rgbif) #fetch data
+library(cleanR) #add missing cols
+library(tidyverse) #edit data structure
 
-########################################-
-#Prepare data download-
-########################################-
-#get codes of places and families
-spain_code <- isocodes[grep("Spain", isocodes$name), "code"]
-portugal_code <- isocodes[grep("Portugal", isocodes$name), "code"]
+#Set species key
 apidae_key <- name_backbone(name="Apidae", rank = "family")$usageKey
 andrenidae_key <- name_backbone(name="Andrenidae", rank = "family")$usageKey
 halictidae_key <- name_backbone(name="Halictidae", rank = "family")$usageKey
@@ -65,6 +60,12 @@ occ_count(taxonKey= stenotritidae_key,
 occ_count(taxonKey= melittidae_key, 
           georeferenced=TRUE, 
           country=portugal_code) #14
+
+family_key <- c(apidae_key, andrenidae_key,
+                halictidae_key, colletidae_key,
+                megachilidae_key, 
+                melittidae_key)
+
 #fetch data
 check <-  data.frame(scientificName = NA, decimalLatitude = NA,
                      decimalLongitude = NA,
@@ -75,10 +76,12 @@ check <-  data.frame(scientificName = NA, decimalLatitude = NA,
 
 check <- define_template(check, NA)
 
-family_key <- c(apidae_key, andrenidae_key,
-                halictidae_key, colletidae_key,
-                megachilidae_key, 
-                melittidae_key)
+cols <-  c("scientificName", "decimalLatitude",
+           "decimalLongitude",
+           "family", "genus", "species",
+           "year", "month", "day", "recordedBy",
+           "identifiedBy", "sex",  "stateProvince",
+           "locality", "coordinatePrecision")
 
 dat <-  data.frame(scientificName = NA, decimalLatitude = NA,
                    decimalLongitude = NA,
@@ -87,118 +90,27 @@ dat <-  data.frame(scientificName = NA, decimalLatitude = NA,
                    identifiedBy = NA, sex = NA,  stateProvince = NA,
                    locality = NA,  coordinatePrecision=NA)
 
-#Load data separately
-dat_spain <- dat
-########################################-
-#Fetch Spain data-
-########################################-
 
 for(i in family_key){
-  
-  temp <- occ_search(taxonKey= i, 
-                     return='data', 
-                     hasCoordinate=TRUE,
-                     hasGeospatialIssue=FALSE,
-                     limit=7000, #safe threshold based on rounding up counts above
-                     country = c(spain_code),
-                     fields = c('scientificName','decimalLatitude',
-                                'decimalLongitude',
-                                'family','genus', 'species',
-                                'year', 'month', 'day', 'recordedBy',
-                                'identifiedBy', 'sex', 'stateProvince', 
-                                'locality', 'coordinatePrecision'))
+  temp <- occ_data(taxonKey= i, 
+                   hasCoordinate=TRUE,
+                   hasGeospatialIssue=FALSE,
+                   limit=10000, 
+                   country = c("ES","PT"))
   
   #Convert to dataframe and add cols if necessary
-  temp <- as.data.frame(temp$data)
-  temp <- add_missing_variables(check, temp)
-  #store the data
-  dat_spain <- rbind(dat_spain, temp)
+  temp_ES <- as.data.frame(temp$ES$data) 
+  temp_ES = temp_ES %>% select(any_of(cols))
+  temp_PT <- as.data.frame(temp$PT$data)
+  temp_PT = temp_PT %>% select(any_of(cols)) 
+  #Add missing vars
+  temp_ES <- add_missing_variables(check, temp_ES)
+  temp_PT <- add_missing_variables(check, temp_PT)
+  #Rbind forloop results
+  dat <- rbind(dat, temp_ES, temp_PT)
   
-} 
+}
 
-#Delete first row with full NA's
-dat_spain <- dat_spain[-1,]
-
-#check if max number is right
-dat_spain %>% 
-  group_by(family) %>%
-  summarise(no_rows = length(family))
-#7000 as a max for gbif seems ok but this may need to be increased in the next year or so
-head(dat_spain)
-tail(dat_spain)
-dim(dat_spain) #16146 records 14/02/2022
-dat_spain$Country <- "Spain" 
-#The good thing of doing this separately 
-#is that we can add country and is missing for many
-
-########################################-
-#Fetch Portugal data-
-########################################-
-#Exclude coordinate precision for Portugal for now, it is giving trouble...
-#I'll have to check what is happening running one by one, future caveat
-#Seems that there are just few records with coordinate precision overall
-
-check <-  data.frame(scientificName = NA, decimalLatitude = NA,
-                     decimalLongitude = NA,
-                     family = NA, genus = NA, species = NA,
-                     year = NA, month = NA, day = NA, recordedBy = NA,
-                     identifiedBy = NA, sex = NA,  stateProvince = NA,
-                     locality = NA)
-
-check <- define_template(check, NA)
-
-dat <-  data.frame(scientificName = NA, decimalLatitude = NA,
-                   decimalLongitude = NA,
-                   family = NA, genus = NA, species = NA,
-                   year = NA, month = NA, day = NA, recordedBy = NA,
-                   identifiedBy = NA, sex = NA,  stateProvince = NA,
-                   locality = NA)
-dat_portugal <- dat
-
-temp <- NULL
-
-for(i in family_key){
-  
-  temp <- occ_search(taxonKey= i, 
-                     return='data', 
-                     hasCoordinate=TRUE,
-                     hasGeospatialIssue=FALSE,
-                     limit=7000, #safe threshold based on rounding up counts above
-                     country = c(portugal_code),
-                     fields = c('scientificName','decimalLatitude',
-                                'decimalLongitude',
-                                'family','genus', 'species',
-                                'year', 'month', 'day', 'recordedBy',
-                                'identifiedBy', 'sex', 'stateProvince', 
-                                'locality'))
-  
-  #Convert to dataframe and add cols if necessary
-  temp <- as.data.frame(temp$dat)
-  temp <- add_missing_variables(check, temp)
-  #store the data
-  dat_portugal <- rbind(dat_portugal, temp)
-  
-} 
-
-#Delete first row with full NA's
-dat_portugal <- dat_portugal[-1,]
-
-#check if max number is right
-dat_portugal %>% 
-  group_by(family) %>%
-  summarise(no_rows = length(family))
-#7000 as a max for gbif seems ok but this may need to be increased in the next year or so
-head(dat_portugal)
-tail(dat_portugal)
-dim(dat_portugal) #16146 records 14/02/2022
-#Add country
-dat_portugal$Country <- "Portugal"
-dat_portugal$coordinatePrecision <- NA #This was giving trouble!
-colnames(dat_spain)
-colnames(dat_portugal)
-
-#Unify gbif data for Spain and Portugal
-dat <- rbind(dat_spain, dat_portugal)
 #Add unique identifier
 dat$uid <-  paste("55_Gbif_", 1:nrow(dat), sep = "")
 
@@ -212,6 +124,7 @@ bounds <- c(35.67, -10.13, 44.15, 4.76) #Iberian peninsula
 #Divide it for apidae because we reach the max number of records
 bound_1 <- c(35.67, -10.13, 44.15, -2.99) #Iberian peninsula in half
 bound_2 <- c(35.67, -3.00, 44.15, 4.76) #Iberian peninsula in half
+#Apidae is divided in two in order to get the max records of the Iberia peninsula
 apidae_1 <- get_inat_obs(taxon_name = "Apidae", geo = TRUE, maxresults = 9999 , bounds = bound_1)
 apidae_2 <- get_inat_obs(taxon_name = "Apidae", geo = TRUE, maxresults = 9999 , bounds = bound_2)
 andrenidae <- get_inat_obs(taxon_name = "Andrenidae", geo = TRUE, maxresults = 9999 , bounds = bounds)
@@ -220,7 +133,6 @@ colletidae <- get_inat_obs(taxon_name = "Colletidae", geo = TRUE, maxresults = 9
 megachilidae <- get_inat_obs(taxon_name = "Megachilidae", geo = TRUE, maxresults = 9999 , bounds = bounds)
 melittidae <- get_inat_obs(taxon_name = "Melittidae", geo = TRUE, maxresults = 9999 , bounds = bounds)
 
-#This needs to be fixed, at the moment we are losing species of Apidae because of the max limit
 inat <- rbind(apidae_1,apidae_2, andrenidae, halictidae, colletidae, megachilidae)
 unique(inat$scientific_name) #need to clean data, a couple subsp.
 head(inat)
@@ -241,14 +153,20 @@ inat$day <- date$mday #extract the day only
 inat$month <- date$mon+1 #extract the day only
 inat$year <- date$year + 1900 #extract the day only
 inat$uid <- paste("55_inaturalist_", 1:nrow(inat), sep = "")
-inat$Country <- NA
 
-d <- rbind(dat[,c("species", "decimalLatitude",  "decimalLongitude", "family",
-                  "year", "month",  "day", "recordedBy", "identifiedBy", "sex",
-                  "stateProvince", "locality", "coordinatePrecision", "uid", "Country")], 
-           inat[, c("species", "decimalLatitude",  "decimalLongitude", "family",
-                    "year", "month",  "day", "recordedBy", "identifiedBy", "sex",
-                    "stateProvince", "locality", "coordinatePrecision", "uid", "Country")])
+d <- rbind(dat[,c("decimalLatitude",
+                  "decimalLongitude",
+                  "family", "species",
+                  "year", "month", "day", "recordedBy",
+                  "identifiedBy", "sex",  "stateProvince",
+                  "locality", "coordinatePrecision", "uid")], 
+           inat[, c("decimalLatitude",
+                    "decimalLongitude",
+                    "family", "species",
+                    "year", "month", "day", "recordedBy",
+                    "identifiedBy", "sex",  "stateProvince",
+                    "locality", "coordinatePrecision", "uid")])
+
 
 #Clean and merge Gbig and inat----
 #species in canary islands
@@ -263,4 +181,5 @@ unique(d4$species) #591 sp ... not bad...some subspecies...
 
 #Save data
 write.csv(d4, file = "Data/Processing_raw_data/online_data.csv")
+
 
