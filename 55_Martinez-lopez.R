c01a1bfca291ed89c47e3e9925d48c8ad64320d0
Jose Barragán Sánchez-Lanuza <barragansljose@gmail.comcd>
2022-03-17 12:19:45 +0100

    martinez-lopez script

---
+++ 55_Martinez-lopez.R
@@ -0,0 +1,6 @@
+source("Scripts/1_2_Processing_raw_data/Source_file.R") #Generate template
+
+# 55_Martinez-lopez  ----
+
+#Read data
+newdat <- read.csv(file = "Data/Rawdata/csvs/55_Martinez-lopez.csv", sep = ";")
