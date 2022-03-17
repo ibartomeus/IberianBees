39a055693d74e181cc513abfd934932a819c436d
Jose Barragán Sánchez-Lanuza <barragansljose@gmail.com>
2022-03-17 13:22:16 +0100

    Dataset 55_Martinez-Lopez already processed

--- 55_Martinez-lopez.R
+++ 55_Martinez-lopez.R
@@ -3,4 +3,37 @@ source("Scripts/1_2_Processing_raw_data/Source_file.R") #Generate template
 # 55_Martinez-lopez  ----
 
 #Read data
-newdat <- read.csv(file = "Data/Rawdata/csvs/55_Martinez-lopez.csv", sep = ";")
+newdat <- read.csv(file = "Data/Rawdata/csvs/55_Martinez-lopez.csv", sep=",")
+
+#Check vars
+compare_variables(check, newdat)
+
+#Rename cols
+colnames(newdat)[which(colnames(newdat) == 'Coordinate.precision..e.g..GPS...10km.')] <- 'Coordinate.precision'
+colnames(newdat)[which(colnames(newdat) == 'month')] <- 'Month'
+colnames(newdat)[which(colnames(newdat) == 'day')] <- 'Day'
+colnames(newdat)[which(colnames(newdat) == 'End.Date')] <- 'End.date'
+colnames(newdat)[which(colnames(newdat) == 'Determiner')] <- 'Determined.by'
+colnames(newdat)[which(colnames(newdat) == 'Reference..doi.')] <- 'Reference.doi'
+colnames(newdat)[which(colnames(newdat) == 'Collection.Location_ID')] <- 'Local_ID'
+
+#Add missing vars
+newdat <- add_missing_variables(check, newdat)
+newdat <- drop_variables(check, newdat) #reorder and drop variables
+
+#Fix wrong province "Castilla-La Mancha", rename if possible
+newdat$Province[newdat$Locality=="Fuente de la Carrasca (Albacete)"] <- "Albacete"
+# Rename Murcia to Murcia following standar names 
+#https://www.ine.es/daco/daco42/codmun/cod_provincia.htm
+newdat$Province[newdat$Province=="Región de Murcia"] <- "Murcia"
+
+#Delete degree symbol of coordinates
+newdat$Latitude <- gsub("°", "", newdat$Latitude)
+newdat$Longitude <- gsub("°", "", newdat$Longitude)
+
+#Standardize unindetified plant species
+newdat$Flowers.visited <- gsub("sp.", "sp", newdat$Flowers.visited, fixed = T)
+newdat$Flowers.visited <- gsub("sp2", "sp", newdat$Flowers.visited, fixed = T)
+
+
+
