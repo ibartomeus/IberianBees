36d897b1ec47fbc76641e8f06f52748f2ea8b4f6
Jose Barragán Sánchez-Lanuza <barragansljose@gmail.com>
2022-03-17 16:52:41 +0100

    to check file updated

--- Scripts/3_2_To_check_sp_names.R
+++ Scripts/3_2_To_check_sp_names.R
@@ -3,11 +3,12 @@
 #Two step process:
 
 #1) Read to_check, select unique cases and check. Ask Thomas Wood if cannot be solved easily.
+#IMPORTANT: This csv is updated manuallly by us.
 
 #2) Append checked species or add the full dataset checked to manual checks
-#For security reasons we keep track of all manual checks. They will be stored on different files
-#For instance, manual_checks.csv, manual_checks_1.csv, manual_checks_2.csv
-#An update of this on 2_Final_cleaning will be done when big changes are produced
+
+#3) RUN Scripts/3_1_Final_cleaning.R and to_checks is automatially updated
+#it should have 0 records after all species has bee checked
 
 library(tidyverse)
 #List of species to check
@@ -15,16 +16,6 @@ to_check <- read.csv("Data/Processing_iberian_bees_raw/to_check.csv", row.names 
 
 #Select unique cases
 to_check <- distinct(to_check) #145 species 
-#To check by version
-#write.csv2(to_check, "Data/Processing_iberian_bees_raw/To_check_by_version/to_check_1.0.csv")
-
-#Load checked species here and append to manual_checks
-#Read manual checks
-to_check_1.0 <- read.csv("Data/Processing_iberian_bees_raw/To_check_by_version/to_check_1.0.csv", sep = ",", stringsAsFactors = FALSE, 
-                   na.strings = c(NA, ""), row.names = 1)
-
-#All versions of to check would be added here 
-#Although this has been done already we starts with the count now
 
 #Now load manual checks and append
 #Read manual checks
@@ -32,10 +23,7 @@ manual <- read.csv("Data/Processing_iberian_bees_raw/manual_checks.csv", sep = "
                    na.strings = c(NA, ""))
 
 #cbind dataframes
-manual <- dplyr::bind_rows(manual, to_check_1.0)
+manual <- dplyr::bind_rows(manual, to_check)
 
 #Save new manual checks
-#write.csv2(manual, "Data/Processing_iberian_bees_raw/manual_checks.csv")
-#To have a record of all manual checks
-#The first manual checks is considered as 0.0 (see folder)
-#write.csv2(manual, "Data/Processing_iberian_bees_raw/Manual_checks_by_version/Manual_checks_1.0.csv")
+write.csv2(manual, "Data/Processing_iberian_bees_raw/manual_checks.csv")
