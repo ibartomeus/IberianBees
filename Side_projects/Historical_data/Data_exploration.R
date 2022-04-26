bf8d5bde15e0e467cc70d7da030ef3fee416a7ed
Jose Barragán Sánchez-Lanuza <barragansljose@gmail.com>
2022-04-26 19:06:25 +0200

    Historical data

---
+++ Side_projects/Historical_data/Data_exploration.R
@@ -0,0 +1,24 @@
+
+#Load library
+library(tidyverse)
+
+#Load data
+data <- read.table("Data/iberian_bees.csv.gz",  header=T, quote="\"", sep=",",row.names=1)
+
+#Filter data (all records older than 1990 for now)
+
+older_1990 <- data %>% filter(Year < 1990)
+
+
+#Number of levels of locality (this may give an idea of the locations with most records)
+localities <- older_1990 %>% 
+  group_by(Locality) %>%
+  summarise(no_rows = length(Locality))
+
+#We need to filter locations with unique records
+#Maybe unique levels of species by location...
+
+s <- older_1990 %>% count(Accepted_name, Locality, sort = TRUE)
+
+
+
